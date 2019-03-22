c356004c286bd3057999dca0a8caafbfca896f9f
Gigi Law <gigi.law.14@gmail.com>
2019-03-22 11:17:21 +0800

    Dummy data

--- src/components/store/reducers/projectReducer.js
+++ src/components/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+	projects: [
+		{ id: '1', title: 'AAA', content: 'lalala' },
+		{ id: '2', title: 'BBB', content: 'hahaha' },
+		{ id: '3', title: 'CCC', content: 'yayaya' },
+	],
+}
 
 const projectReducer = (state = initState, action) => {
 	return state
