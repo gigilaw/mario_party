4e49287cc1768c202019caf3d5599a272853479b
Gigi Law <gigi.law.14@gmail.com>
2019-03-23 10:47:45 +0800

    Firestore setup

--- src/components/store/actions/projectActions.js
+++ src/components/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = project => {
-	return (dispatch, getState) => {
+	return (dispatch, getState, { getFirebase, getFireStore }) => {
 		//async
 		dispatch({ type: 'CREATE_PROJECT', project })
 	}
