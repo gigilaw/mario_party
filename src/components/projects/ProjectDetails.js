cf72daf8dd736246028519f994221e96e93454da
Gigi Law <gigi.law.14@gmail.com>
2019-03-25 12:50:10 +0800

    Dynamic project detail

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,26 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails = props => {
-	const id = props.match.params.id
-	return (
-		<div className="container section project-details">
-			<div className="card z-depth-0">
-				<div className="card-content">
-					<span className="card-title">Project Title - {id}</span>
-					<p>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus,
-						voluptatibus omnis? Doloremque optio et minus. Soluta, voluptate
-						similique. Ratione quo inventore a, magnam sunt explicabo recusandae
-						omnis quas odio praesentium!
-					</p>
-				</div>
-				<div className="card-action lighten-4 grey-text">
-					<div>Posted By Gigi</div>
-					<div>Today</div>
+	const { project } = props
+	if (project) {
+		return (
+			<div className="container section project-details">
+				<div className="card z-depth-0">
+					<div className="card-content">
+						<span className="card-title">{project.title}</span>
+						<p>{project.content}</p>
+					</div>
+					<div className="card-action lighten-4 grey-text">
+						<div>
+							Posted By {project.authorFirstName} {project.authorLastName}
+						</div>
+						<div>Today</div>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	} else {
+		return (
+			<div className="container center">
+				<p>Loading project...</p>
+			</div>
+		)
+	}
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, ownProps) => {
+	console.log(ownProps)
+	const id = ownProps.match.params.id
+	const projects = state.firestore.data.projects
+	const project = projects ? projects[id] : null
+	return {
+		project: project,
+	}
+}
+export default compose(
+	connect(mapStateToProps),
+	firestoreConnect([{ collection: 'projects' }]),
+)(ProjectDetails)
